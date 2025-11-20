3e5eb2a6ba7ec7132e8b14a8157e25fd3e485747
alimran halmamat <mr.uyghur1996@gmail.com>
2025-11-19 23:55:37 -0800

    added leetcode dashboard link

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,5 @@
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
@@ -14,6 +16,7 @@ function Main() {
           <div className="social_icons">
             <a href="https://github.com/mr-uyghur" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/ali-h-4370361b4/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://leetcode.com/u/AltairOmega/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faCode} size="lg" /></a>
           </div>
           <h1>Ali Halmamat</h1>
           <p>Full Stack Engineer</p>
@@ -21,6 +24,7 @@ function Main() {
           <div className="mobile_social_icons">
             <a href="https://github.com/mr-uyghur" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/ali-h-4370361b4/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://leetcode.com/u/AltairOmega/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faCode} size="lg" /></a>
           </div>
         </div>
       </div>
